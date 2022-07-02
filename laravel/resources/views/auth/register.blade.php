67bc937786737fc11eb2851c709c519e9a55dbed
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2022-07-02 13:57:17 +0900

    2-5 バサー登録ボタンの色を変更

--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
                 @csrf
                 <div class="md-form">
                   <label for="name">ユーザー名</label>
-                  <input class="from-control" type="text" id="name" name="name" required value="{{ old('name') }}">
+                  <input class="from-control" type="text" id="name" name="name" required value="{{ old('name') }}"><br>
                   <small>英数字3~16文字(登録後の変更不可)</small>
                 </div>
                 <div class="md-form">
@@ -31,7 +31,7 @@
                   <label for="password-confirmation">パスワード(確認)</label>
                   <input class="from-control" type="password" id="confirmation" name="confirmation" required>
                 </div>
-                <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">バサー登録</button>
+                <button class="btn btn-block dusty-grass-gradient mt-2 mb-2" type="submit">バサー登録</button>
               </form>
               <div class="mt-0">
                 <a href="{{ route('login') }}" class="card-text">ログインはこちら</a>
