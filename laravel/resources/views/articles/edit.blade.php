fd1528c67442bc77ea522f3d65dd102785e4b656
KenTanaka <k.tanaka@apptime.co.jp>
2022-07-06 18:55:35 +0900

    5-2 UI変更

--- laravel/resources/views/articles/edit.blade.php
+++ laravel/resources/views/articles/edit.blade.php
@@ -15,7 +15,7 @@
               <form method="POST" action="{{ route('articles.update', ['article' => $article->id]) }}">
                 @method('PATCH')
                 @include('articles.form')
-                <button type="submit" class="btn blue-gradient btn-block">更新する</button>
+                <button type="submit" class="btn dusty-grass-gradient btn-block">更新する</button>
               </form>
             </div>
           </div>
