9aa538fd14177ccd6c38242c0ef4b2fc705a964b
KenTanaka <k.tanaka@apptime.co.jp>
2022-07-06 14:40:44 +0900

    4-2 CRUDルーティングの追加

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -15,5 +15,13 @@
 //     return view('welcome');
 // });
 
+// ログイン機能
 Auth::routes();
-Route::get('/', 'ArticleController@index');
+
+// 記事一覧画面(TOPページ)
+Route::get('/', 'ArticleController@index')->name('articles.index');
+
+// よく使われる機能のルーティング
+// store, index, create, destroy, update, show, edit
+// docker-compose exec workspace php artisan route:list (laradockディレクトリ内で確認)
+Route::resource('/articles', 'ArticleController')->except(['index']);
