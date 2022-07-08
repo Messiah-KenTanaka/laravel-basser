117d296b3e4f86227e4ec17abd9d3b47f6945fdc
KenTanaka <k.tanaka@apptime.co.jp>
2022-07-08 16:33:24 +0900

    5-5 記事更新と記事削除への認可

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
+    }
     /**
     * バサー一覧画面
     * /
